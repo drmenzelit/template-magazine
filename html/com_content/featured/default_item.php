0780d29ccb0db46c70bbbc4febdbf80ccb2ee2fe
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2015-10-14 12:04:39 +0200

    Changes on featured articles override for multicolumn support

--- html/com_content/featured/default_item.php
+++ html/com_content/featured/default_item.php
@@ -62,9 +62,9 @@ $info    = $this->item->params->get('info_block_position', 0);
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
 	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
 	<?php if ($images->image_intro_caption):
-		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
+		echo 'class="caption img-responsive"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
 	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/> </div>
+	src="<?php echo htmlspecialchars($images->image_intro); ?>" class="img-responsive" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/> </div>
 <?php endif; ?>
 
 <?php if (!$params->get('show_intro')) : ?>
